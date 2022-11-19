b4e72d5b1d4721377845f6f237b38dff7c4d6d96
kim-yeji <2214yjgm@gmail.com>
2022-11-19 12:16:47 +0900

    fix: external network not found 현상 수정

--- ops/end.sh
+++ ops/end.sh
@@ -1,13 +1,12 @@
 echo "\n\n========== end routine start! ==========\n\n"
 
 echo "\n\n========== step 1. backend down! ==========\n\n"
-docker-compose -f docker-compose.backend.yaml down
+docker-compose -f docker-compose.app.yaml down
 echo "\n\n========== step 1. finish ==========\n\n"
 
 echo "\n\n========== step 2. databases down! ==========\n\n"
 docker-compose -f docker-compose.database.yaml down
 echo "\n\n========== step 2. finish ==========\n\n"
-sleep 2
 
 echo "\n\n========== step 3. check! ==========\n\n"
 docker ps
